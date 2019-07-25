8371c58808f5322fff084585c781ee5bbc704f30
anxing <anxing131@gmail.com>
2019-07-25 21:58:32 +0800

    - fixed grammar issue

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   s3: {
     SEND_TO_CLOUD: process.env.SEND_TO_CLOUD || false,
     MINIO_END_POINT:  process.env.MINIO_END_POINT || '',
-    MINIO_PORT:  process.env.MINIO_END_POINT || 9000,
+    MINIO_PORT:  process.env.MINIO_PORT || 9000,
     MINIO_ACCESS_KEY:  process.env.MINIO_ACCESS_KEY || '',
     MINIO_SECRET_KEY:  process.env.MINIO_SECRET_KEY || '',
     MINIO_SAVE_BUCKET:  process.env.MINIO_SAVE_BUCKET || 'xieluntest',
