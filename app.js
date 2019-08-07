b376f06af7f5b33d8919bc57f13c7b399ea6b01f
anxing <anxing131@gmail.com>
2019-08-07 18:38:15 +0800

    - fixed cors grammar issue

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
